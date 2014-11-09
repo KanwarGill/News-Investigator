6c8f78c5d62a089e8aac4953bd2c3b8e6af85187
masamson <masamson@mail.utoronto.ca>
2014-11-09 15:21:14 -0500

    Created ajax call to the backend.

--- Website/c01/js/main.js
+++ Website/c01/js/main.js
@@ -1,127 +1,164 @@
-          $("#btnAddSource").click(function () {
-          //var urlRegex = new RegExp("/^(https?://)?([da-z.-]+).([a-z.]{2,6})([/w .-]*)*/?$/");
-	  var urlRegex = new RegExp("^w{3}\.[a-zA-Z0-9]{2,}\.[a-z]{2,3}$");
-          var source = $("#inputsource").val();
-           if (!urlRegex.test(source)) {
-             alert("Please enter a valid url");
-	     return;
-           }
-          var id = source.substring(source.indexOf(".") + 1, source.lastIndexOf("."));
-          // console.log(id);
-          $.ajax({
-              type: "POST",
-              url: "http://www.chihuahuas.iriscouch.com/news_source",
-              dataType: "json",
-              data: '{ "_id" : "' + id + '", "url": "' + source + '" }',
-              contentType: "application/json",
-              processData: false,
-              success: function (data) {
-                alert("Successfully added " + source);
-              },
-              error: function(){
-                alert("Cannot add duplicate news source");
-              }
-          });
-        });
+$("#btnAddSource").click(function() {
+  //var urlRegex = new RegExp("/^(https?://)?([da-z.-]+).([a-z.]{2,6})([/w .-]*)*/?$/");
+  var urlRegex = new RegExp("^w{3}\.[a-zA-Z0-9]{2,}\.[a-z]{2,3}$");
+  var source = $("#inputsource").val();
+  if (!urlRegex.test(source)) {
+      alert("Please enter a valid url");
+      return;
+  }
+  var id = source.substring(source.indexOf(".") + 1, source
+      .lastIndexOf("."));
+  // console.log(id);
+  $.ajax({
+      type: "POST",
+      url: "http://www.chihuahuas.iriscouch.com/news_source",
+      dataType: "json",
+      data: '{ "_id" : "' + id + '", "url": "' +
+          source + '" }',
+      contentType: "application/json",
+      processData: false,
+      success: function(data) {
+          alert("Successfully added " + source);
+      },
+      error: function() {
+          alert(
+              "Cannot add duplicate news source"
+          );
+      }
+  });
+});
 
-        $("#btnGetSources").click(function() {
-          $(".getsources").empty();
-          // Display the current sources form the DB
-          $.getJSON("http://www.chihuahuas.iriscouch.com/news_source/_all_docs?include_docs=true", function(data) {
-            var sources = [];
-            $.each(data.rows, function(key, val) {
+$("#btnGetSources").click(function() {
+  $(".getsources").empty();
+  // Display the current sources form the DB
+  $.getJSON(
+      "http://www.chihuahuas.iriscouch.com/news_source/_all_docs?include_docs=true",
+      function(data) {
+          var sources = [];
+          $.each(data.rows, function(key, val) {
               // console.log(val.doc);
-              sources.push("<li id='" + val.doc._id + "'>" + val.doc.url + "</li>");
-            });
+              sources.push("<li id='" + val.doc
+                  ._id + "'>" + val.doc.url +
+                  "</li>");
+          });
 
-            $("<ul/>", {
+          $("<ul/>", {
               "class": "listofsources",
               html: sources.join("")
-            }).appendTo(".getsources");
-          });
-        });
+          }).appendTo(".getsources");
+      });
+});
 
-        $("#btnDeleteSource").click(function () {
-          var source = $("#deletesource").val();
-          var id = source.substring(source.indexOf(".") + 1, source.lastIndexOf("."));
-          var rev = "";
-          $.getJSON("http://www.chihuahuas.iriscouch.com/news_source/" + id, function(data) {
-            $.each(data, function(key, val) {
+$("#btnDeleteSource").click(function() {
+  var source = $("#deletesource").val();
+  var id = source.substring(source.indexOf(".") + 1, source
+      .lastIndexOf("."));
+  var rev = "";
+  $.getJSON(
+      "http://www.chihuahuas.iriscouch.com/news_source/" +
+      id, function(data) {
+          $.each(data, function(key, val) {
               rev = data._rev;
-            });
-            $.ajax({
-              type: "DELETE",
-              url: "http://www.chihuahuas.iriscouch.com/news_source/" + id + "?rev=" + rev,
-              dataType: "json",
-              contentType: "application/json",
-              success: function (data) {
-                alert("Successfully deleted " + source);
-              },
-              error: function(){
-                alert("ERROR: Cannot get keyword");
-              }
-            });
           });
-        });
-
-        $("#btnAddKeyword").click(function () {
-          var key = $("#inputkeyword").val();
-	  var keyRegex = new RegExp("^[a-zA-Z0-9]+$");
-           if (!keyRegex.test(key)) {
-             alert("Please enter an alphanumeric keyword");
-	     return;
-           }	
           $.ajax({
-              type: "POST",
-              url: "http://www.chihuahuas.iriscouch.com/keywords/",
+              type: "DELETE",
+              url: "http://www.chihuahuas.iriscouch.com/news_source/" +
+                  id + "?rev=" + rev,
               dataType: "json",
-              data: '{ "_id" : "' + key + '", "keyword" : "' + key + '" }',
               contentType: "application/json",
-              processData: false,
-              success: function (data) {
-                alert("Successfully added " + key);
+              success: function(data) {
+                  alert(
+                      "Successfully deleted " +
+                      source);
               },
-              error: function(){
-                alert("Cannot add duplicate keyword");
+              error: function() {
+                  alert(
+                      "ERROR: Cannot get keyword"
+                  );
               }
           });
-        });
+      });
+});
 
-        $("#btnGetKeywords").click(function() {
-          $(".getkeywords").empty();
-          // Display the current keywords form the DB
-          $.getJSON("http://www.chihuahuas.iriscouch.com/keywords/_all_docs?include_docs=true", function(data) {
-            var keywords = [];
-            $.each(data.rows, function(key, val) {
+$("#btnAddKeyword").click(function() {
+  var key = $("#inputkeyword").val();
+  var keyRegex = new RegExp("^[a-zA-Z0-9]+$");
+  if (!keyRegex.test(key)) {
+      alert("Please enter an alphanumeric keyword");
+      return;
+  }
+  $.ajax({
+      type: "POST",
+      url: "http://www.chihuahuas.iriscouch.com/keywords/",
+      dataType: "json",
+      data: '{ "_id" : "' + key +
+          '", "keyword" : "' + key + '" }',
+      contentType: "application/json",
+      processData: false,
+      success: function(data) {
+          alert("Successfully added " + key);
+      },
+      error: function() {
+          alert("Cannot add duplicate keyword");
+      }
+  });
+});
+
+$("#btnGetKeywords").click(function() {
+  $(".getkeywords").empty();
+  // Display the current keywords form the DB
+  $.getJSON(
+      "http://www.chihuahuas.iriscouch.com/keywords/_all_docs?include_docs=true",
+      function(data) {
+          var keywords = [];
+          $.each(data.rows, function(key, val) {
               // console.log(val.doc);
-              keywords.push("<li class='keywords'>" + val.doc.keyword + "</li>");
-            });
+              keywords.push(
+                  "<li class='keywords'>" +
+                  val.doc.keyword + "</li>"
+              );
+          });
 
-            $("<ul/>", {
+          $("<ul/>", {
               "class": "listofkeywords",
               html: keywords.join("")
-            }).appendTo(".getkeywords");
-          });
-        });
+          }).appendTo(".getkeywords");
+      });
+});
 
-        $("#btnDeleteKeyword").click(function () {
-          var key = $("#deletekeyword").val();
-          var rev = "";
-          $.getJSON("http://www.chihuahuas.iriscouch.com/keywords/" + key, function(data) {
-            $.each(data, function(key, val) {
+$("#btnDeleteKeyword").click(function() {
+  var key = $("#deletekeyword").val();
+  var rev = "";
+  $.getJSON("http://www.chihuahuas.iriscouch.com/keywords/" +
+      key, function(data) {
+          $.each(data, function(key, val) {
               rev = data._rev;
-            });
-            $.ajax({
+          });
+          $.ajax({
               type: "DELETE",
-              url: "http://www.chihuahuas.iriscouch.com/keywords/" + key + "?rev=" + rev,
+              url: "http://www.chihuahuas.iriscouch.com/keywords/" +
+                  key + "?rev=" + rev,
               dataType: "json",
               contentType: "application/json",
-              success: function (data) {
-                alert("Successfully deleted " + key);
+              success: function(data) {
+                  alert(
+                      "Successfully deleted " +
+                      key);
               },
-              error: function(){
-                alert("ERROR: Cannot get keyword");
+              error: function() {
+                  alert(
+                      "ERROR: Cannot get keyword"
+                  );
               }
-            });
           });
-        });
+      });
+});
+
+$("#btnStartCrawl").click(function() {
+    console.log("YAYs!");
+    $.post("crawlmanagement.py",
+            {action: "crawl"},
+            function(result) {
+                console.log(result);
+            });
+});
--- Website/flaskVersion/app/views.py
+++ Website/flaskVersion/app/views.py
@@ -1,5 +1,10 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import app
+from twisted.internet import reactor
+from scrapy.crawler import Crawler
+from scrapy import log, signals
+from webcrawl.spiders.aljazeera_spider2 import FollowAllSpider
+from scrapy.utils.project import get_project_settings
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +24,21 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
+                           
+@app.route('/crawl')
+def crawl():
+    return render_template("crawl.html",
+                           title='Crawl')
+
+@app.route('/crawling', methods=['POST'])
+def crawling():
+    spider = FollowAllSpider(domain='scrapinghub.com')
+    settings = get_project_settings()
+    crawler = Crawler(settings)
+    crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+    crawler.configure()
+    crawler.crawl(spider)
+    crawler.start()
+    log.start()
+    reactor.run() # the script will block here until the spider_closed signal was sent
+    return jsonify({'result': 'Connected'})
