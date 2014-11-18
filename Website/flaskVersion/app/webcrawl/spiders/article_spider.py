46a50051a768ea45e46d4b15b7caedc0289743ea
Kanwar245 <champ.kanwar@gmail.com>
2014-11-17 23:32:41 -0500

    Add the general article spider

---
+++ Website/flaskVersion/app/webcrawl/spiders/article_spider.py
@@ -0,0 +1,66 @@
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.selector import HtmlXPathSelector
+from ..items import ArticleItem
+from scrapy.http import Request
+import couchdb
+import re
+from urlparse import urlparse
+
+class ArticleSpider(CrawlSpider):
+    
+    # List of the start URLs
+    start_urls = []
+    # List of the allowed domains
+    allowed_domains = []
+    
+    # Access CouchDB database
+    couch = couchdb.Server('http://chihuahuas.iriscouch.com:5984/')
+    db = couch['news_investigator']
+    # Grab all urls from the news_source database
+    for row in db.view('byDocType/byNewsSource'):
+        parsed = urlparse(row.key)
+        # append the urls
+        start_urls.append(row.key)
+        # append the allowed domains
+        if parsed.netloc:
+            # if the url begins with rss, replace it with www
+            if re.match('rss\..*\..*', parsed.netloc):
+                new = parsed.netloc.encode("ascii", "ignore")
+                n2 = new.split('.')
+                # remove the rss item
+                n2.pop(0)
+                # insert the www item
+                n2.insert(0, 'www')
+                # add the new url to the allowed domains
+                allowed_domains.append(".".join(n2))
+        else:
+            allowed_domains.append(parsed.path)
+    
+    name = "article"
+    
+    def parse(self, response):
+        for article in response.xpath('//channel/item'):
+            item = ArticleItem()
+            # Grab the title and the link to the article
+            item ["title"] = article.xpath("title/text()").extract()
+            item ["link"] = article.xpath("link/text()").extract()
+            item ["date"] = article.xpath("pubDate/text()").extract()
+            
+            link = item["link"][0]
+            # Grab the source of the page by making another Request
+            yield Request(link,callback = self.parse_link, meta = dict(item = item))
+                
+    def parse_link(self, response):
+        self.log('Grabbing source from %s' % response.url)
+        hxs = HtmlXPathSelector(response)
+        item = response.meta.get('item')
+        # The articles in Al Jazeera is under the td tag,
+        # Detailed Summary class
+        # item ["source"] = hxs.select('//td[@class="DetailedSummary"]/p').extract()
+        item ["source"] = hxs.select('p').extract()
+        
+        # Extract hyperlinks, extract quotes, extract mentions here
+        
+        return item
+
+
