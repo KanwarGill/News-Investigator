01de15e3b6cd2a569d7cf325491217ce05c0b778
Kanwar245 <champ.kanwar@gmail.com>
2014-11-25 20:17:37 -0500

    Modiy twitter_crawl.py to update the document if it exists already

--- Website/flaskVersion/app/twitter_crawl.py
+++ Website/flaskVersion/app/twitter_crawl.py
@@ -10,16 +10,16 @@ db = couch['test_news_investigator']
 
 def twitter_crawl():
     # authentication for Twitter API
-    #auth = tweepy.OAuthHandler('PNIJuzLXezWr3xrxScEbA68l3', 
-                        #'Li7Z5ZOvIu4o50G0n2wEmv5Nj0JmsCvSjR7OUx4CMompnIqGjH')
-    #auth.set_access_token('64169290-OZeLtehY0eSBkj12ebWZlYP8KtzlJtxLF5DEM7Jb8', 
-                          #'WbgHw8xyDJ77ZqWKDjs9B2rzEKzAuHcBFrO04xB9bgNqo')
+    auth = tweepy.OAuthHandler('PNIJuzLXezWr3xrxScEbA68l3', 
+                        'Li7Z5ZOvIu4o50G0n2wEmv5Nj0JmsCvSjR7OUx4CMompnIqGjH')
+    auth.set_access_token('64169290-OZeLtehY0eSBkj12ebWZlYP8KtzlJtxLF5DEM7Jb8', 
+                          'WbgHw8xyDJ77ZqWKDjs9B2rzEKzAuHcBFrO04xB9bgNqo')
     
     # include the twitter authentication
-    auth = tweepy.OAuthHandler('XBuKUEBKUIkdWxsJGHapWZp2j', 
-                            '99xPfB2FJxzoDfjxd2Pz0aO6l4wMl7EvqL1osvvP8QpFNPLe9T')
-    auth.set_access_token('2891335167-8KtLExQ2JSqf1dAR1uo4P4Nnao54bXyQJX1RRA0', 
-                          'sUcWM6cx98GP4xuQagmJ6zQr2FoseqUppWnXSCdQbdUec')
+    #auth = tweepy.OAuthHandler('XBuKUEBKUIkdWxsJGHapWZp2j', 
+                            #'99xPfB2FJxzoDfjxd2Pz0aO6l4wMl7EvqL1osvvP8QpFNPLe9T')
+    #auth.set_access_token('2891335167-8KtLExQ2JSqf1dAR1uo4P4Nnao54bXyQJX1RRA0', 
+                          #'sUcWM6cx98GP4xuQagmJ6zQr2FoseqUppWnXSCdQbdUec')
     
     api = tweepy.API(auth)    
     page_num = 0
@@ -33,17 +33,6 @@ def twitter_crawl():
     for handle in db.view('byDocType/byHandle'):
 	handles_list.append(handle.value['handle'])
 	
-    # get the list of current tweet ids
-    for tweet_id in db.view('byDocType/byTweet'):
-	ids = []
-	t_id = (tweet_id.value['_id'])[6:]
-	ids.append(t_id)
-	for handle in handles_list:
-	    # if the handle exists in the ids then remove it
-	    if (handle in ids):
-		db.delete(tweet_id.value)
-		print "Deleted " + t_id
-	
     # loop through the list of handles and save the tweets in the database
     for handle in handles_list:
 	# start from page 0 for each handle
@@ -55,9 +44,13 @@ def twitter_crawl():
 		user_tweets.append(tweet.text)
 	    page_num += 1
 	try:
-	    # save the tweet in the database
+	    # save the tweet in the database if it's a new ID
 	    document = dict(_id="tweet_" + handle, doc_type="tweet", tweet=user_tweets)
 	    db.save(document)
 	    print "Document with id " + _id + "saved"
 	except:
+	    # otherwise update the existing ID
 	    print "ID tweet_" + handle + " already exists"
+	    document = dict(_id="tweet_" + handle, doc_type="tweet", tweet=user_tweets)
+	    db.update([document])
+	    print "Updated"
